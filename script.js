a7d317193ca5698add6154a4696474fb739781aa
MuhammadAbdullah029 <abdullahgu77ar@gmail.com>
2024-04-24 06:52:23 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let tl = gsap.timeline()
+
+tl.from("nav", {
+        y: -100,
+        duration: 1
+})
+
+tl.from(".social", {
+    x: -100,
+    duration: .5
+})
+
+tl.from(".buy h1", {
+    y: -150,
+    duration: 1.5,
+    stagger: .5,
+    ease: "bounce.out"
+})
+
+tl.from(".elem", {
+    x: 330,
+    duration: 3,
+    stagger: .5,
+    ease: "bounce.out"
+})
